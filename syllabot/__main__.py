d613ed0b170ff315bea0902204284105d0bf034a
Khinshan Khan <khanshan501@gmail.com>
2020-06-12 00:06:26 -0400

    QoL

--- syllabot/__main__.py
+++ syllabot/__main__.py
@@ -6,6 +6,8 @@ This module is the heart and entry-point of the syllabot.
 import os
 
 from dotenv import load_dotenv
+import json
+import requests
 
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -13,16 +15,28 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-PREFIXES = os.getenv("PREFIX")
-BOT_PREFIX = tuple(PREFIXES.split()) if PREFIXES else ()
+BOT_PREFIX = os.getenv("PREFIX")
 client = Bot(command_prefix=BOT_PREFIX)
 
+FILES_URL = os.getenv("SYLLABI_FILES")
 
 @client.event
 async def on_ready():
-    """Some commands when the bot has been booted up. Mostly helpful prints."""
     print("Logged in as " + client.user.name)
 
+
+def get_syllabi():
+    """
+    Gets all syllabi from RichAguil/HunterCS_CourseSyllabi.
+
+    HACK depends on RichAguil/HunterCS_CourseSyllabi pdfs to only be
+         syllabi pdfs, this shouldn't matter too much with filters
+         augmented on top of this
+    """
+    res = requests.get(FILES_URL)
+    files = json.loads(res.text)["tree"]
+    relevant_files = filter(lambda file: file["path"].endswith(".pdf"), files)
+    return map(lambda file: {"file": file["path"].split("/")[-1], "url": file["url"]}, relevant_files)
 
 @client.command()
 async def ping(ctx):
@@ -32,7 +46,10 @@ async def ping(ctx):
 
 def main():
     """Execute helpful information about program and then start bot."""
-    client.run(TOKEN)
+    r = get_syllabi()
+    for i in r:
+        print(i)
+    # client.run(TOKEN)
 
 
 if __name__ == '__main__':
